718f7e084bd0b1fb485a72d354b462c93c7d82d1
ky7408 <ky7408@naver.com>
2026-03-30 19:21:53 +0900

    feat: 아바타 컴포넌트

--- jhpjt/app/actions/login.action.ts
+++ jhpjt/app/actions/login.action.ts
@@ -1,32 +1,3 @@
-// "use server";
-// import { API_BASE_URL } from "../utills/api";
-// import { http } from "@/app/api/http";
-
-// type LoginPayload = {
-//   email: string;
-//   password: string;
-// };
-
-// export const postLoginAction = async ({ email, password }: LoginPayload) => {
-//   const res = await http("/api/login", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       email: email,
-//       password: password,
-//     }),
-//   });
-
-//   if (!res.ok) {
-//     const error = await res.json();
-//     throw new Error(error.message ?? "로그인 실패");
-//   }
-
-//   const data = await res.json();
-//   return data;
-// };
 "use server";
 import { http } from "@/app/api/http";
 
---
+++ jhpjt/app/components/Avatar/Avatar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+
+type AvatarSize = "sm" | "md" | "lg";
+
+type AvatarProps = {
+  src?: string;
+  alt?: string;
+  name?: string;
+  size?: AvatarSize;
+  className?: string;
+};
+
+const sizeStyle = {
+  sm: "w-[28px] h-[28px] text-xs",
+  md: "w-8 h-8 text-sm",
+  lg: "w-12 h-12 text-base",
+};
+
+export default function Avatar({
+  src,
+  alt = "프로필 이미지",
+  name,
+  size = "md",
+  className,
+}: AvatarProps) {
+  const fallbackText = name ? name.charAt(0).toUpperCase() : "U";
+
+  return (
+    <div
+      className={`
+  relative overflow-hidden rounded-full
+  flex items-center justify-center
+  bg-gray-400
+  ${sizeStyle[size]}
+  ${className}
+`}
+    >
+      {src ? (
+        <Image src={src} alt={alt} fill className="object-cover" sizes="56px" />
+      ) : (
+        <span className="mt-0.5">{fallbackText}</span>
+      )}
+    </div>
+  );
+}
