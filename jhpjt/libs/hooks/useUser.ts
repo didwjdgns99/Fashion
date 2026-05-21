eaaa7ef360c18a2cf61a9a010c65939eaeffdf62
ky7408 <ky7408@naver.com>
2026-05-21 20:57:40 +0900

    Rollback useUser

--- jhpjt/libs/hooks/useUser.ts
+++ jhpjt/libs/hooks/useUser.ts
@@ -1,13 +1,12 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-// import { getMeAction } from "@/app/actions/getMe.action";
-import { getMeWithDuck } from "@/app/api/getMeWithDuck";
+import { getMeAction } from "@/app/actions/getMe.action";
 
 export function useUser() {
   return useQuery({
     queryKey: ["me"],
-    queryFn: getMeWithDuck,
+    queryFn: getMeAction,
     retry: false,
     staleTime: 0,
     refetchOnMount: true,
