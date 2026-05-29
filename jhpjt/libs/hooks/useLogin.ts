9176b86fa223742ab6e7b1fc890a62cd30f9393c
ky7408 <ky7408@naver.com>
2026-05-29 17:11:29 +0900

    fix:useLoginCode

--- jhpjt/libs/hooks/useLogin.ts
+++ jhpjt/libs/hooks/useLogin.ts
@@ -1,21 +1,22 @@
 "use client";
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { postLoginAction } from "@/app/actions/login.action";
+// import { postLoginAction } from "@/app/actions/login.action";
+import { login, LoginRequest } from "@/app/api/login";
 
-export type LoginRequest = {
-  email: string;
-  password: string;
-};
+// export type LoginRequest = {
+//   email: string;
+//   password: string;
+// };
 
 //로그인 리스폰스값, 에러타입, 페이로드
 export const useLogin = () => {
   const queryClient = useQueryClient();
 
-  return useMutation<unknown, unknown, LoginRequest>({
-    mutationFn: postLoginAction,
+  return useMutation({
+    mutationFn: (payload: LoginRequest) => login(payload),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["me"] }); // 로그인 후 무효화
+      queryClient.invalidateQueries({ queryKey: ["me"] });
     },
   });
 };
