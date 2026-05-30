71b16ec92204c1fdc414170f5185d97eefa7b1d2
ky7408 <ky7408@naver.com>
2026-05-30 14:35:25 +0900

    오픈그래프 생성

--- jhpjt/app/(auth)/signup/page.tsx
+++ jhpjt/app/(auth)/signup/page.tsx
@@ -1,13 +1,16 @@
 import SignupForm from "@/app/containers/Signup/SignupForm";
 import Image from "next/image";
 import Heading_1 from "@/public/image/Heading_1.svg";
+import Link from "next/link";
 
 export default function SignupPage() {
   //회원가입 버튼(폼 제출)
 
   return (
     <div className="flex flex-col items-center px-4 max-w-[480px] mt-[40px] gap-6 m-auto">
-      <Image src={Heading_1} alt="로고" width={180} height={180} />
+      <Link href="/products">
+        <Image src={Heading_1} alt="로고" width={180} height={180} />
+      </Link>
       <SignupForm />
     </div>
   );
